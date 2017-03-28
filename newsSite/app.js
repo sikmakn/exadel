682b0f22a4a4ec902a4653b28c615d01ae8e11ee
КолчановНС <sikmakn@gmail.com>
2017-03-28 22:05:02 +0300

    add charset=utf-8 into server without express

--- newsSite/app.js
+++ newsSite/app.js
@@ -7,7 +7,7 @@ http.createServer(function (req, res) {
 
         fs.readFile(__dirname + '/public/index.html', function (err, data) {
             if (err) console.log(err);
-            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
             res.write(data);
             res.end();
             console.log(req.url);
@@ -17,7 +17,7 @@ http.createServer(function (req, res) {
         if (req.url.indexOf('articleModel') != -1) {
             fs.readFile(__dirname + '/public/articleModel.js', function (err, data) {
                 if (err) console.log(err);
-                res.writeHead(200, {'Content-Type': 'text/javascript'});
+                res.writeHead(200, {'Content-Type': 'text/javascript; charset=utf-8'});
                 res.write(data);
                 res.end();
             });
@@ -25,7 +25,7 @@ http.createServer(function (req, res) {
         if (req.url.indexOf('localStorage') != -1) {
             fs.readFile(__dirname + '/public/localStorage.js', function (err, data) {
                 if (err) console.log(err);
-                res.writeHead(200, {'Content-Type': 'text/javascript'});
+                res.writeHead(200, {'Content-Type': 'text/javascript; charset=utf-8'});
                 res.write(data);
                 res.end();
             });
@@ -33,7 +33,7 @@ http.createServer(function (req, res) {
         if (req.url.indexOf('newsView') != -1) {
             fs.readFile(__dirname + '/public/newsView.js', function (err, data) {
                 if (err) console.log(err);
-                res.writeHead(200, {'Content-Type': 'text/javascript'});
+                res.writeHead(200, {'Content-Type': 'text/javascript; charset=utf-8'});
                 res.write(data);
                 res.end();
             });
@@ -41,7 +41,7 @@ http.createServer(function (req, res) {
         if (req.url.indexOf('clickFunctions') != -1) {
             fs.readFile(__dirname + '/public/clickFunctions.js', function (err, data) {
                 if (err) console.log(err);
-                res.writeHead(200, {'Content-Type': 'text/javascript'});
+                res.writeHead(200, {'Content-Type': 'text/javascript; charset=utf-8'});
                 res.write(data);
                 res.end();
             });
@@ -49,7 +49,7 @@ http.createServer(function (req, res) {
         if (req.url.indexOf('scripts') != -1) {
             fs.readFile(__dirname + '/public/scripts.js', function (err, data) {
                 if (err) console.log(err);
-                res.writeHead(200, {'Content-Type': 'text/javascript'});
+                res.writeHead(200, {'Content-Type': 'text/javascript; charset=utf8'});
                 res.write(data);
                 res.end();
             });
@@ -58,7 +58,7 @@ http.createServer(function (req, res) {
 
         fs.readFile(__dirname + '/public/styles.css', function (err, data) {
             if (err) console.log(err);
-            res.writeHead(200, {'Content-Type': 'text/css'});
+            res.writeHead(200, {'Content-Type': 'text/css; charset=utf-8'});
             res.write(data);
             res.end();
         });
@@ -66,19 +66,19 @@ http.createServer(function (req, res) {
     }else if(req.url=="/images/redactButton.png"){
         fs.readFile(__dirname + '/public/images/redactButton.png', function (err, data) {
             if (err) console.log(err);
-            res.writeHead(200, {'Content-Type': 'image/png'});
+            res.writeHead(200, {'Content-Type': 'image/png; charset=utf-8'});
             res.write(data);
             res.end();
         });
     }else if(req.url=="/images/bucket.png"){
         fs.readFile(__dirname + '/public/images/bucket.png', function (err, data) {
             if (err) console.log(err);
-            res.writeHead(200, {'Content-Type': 'image/png'});
+            res.writeHead(200, {'Content-Type': 'image/png; charset=utf-8'});
             res.write(data);
             res.end();
         });
     }else{
-        res.writeHead(404,{'Content-Type': 'text/css'});
+        res.writeHead(404,{'Content-Type': 'text; charset=utf-8'});
         res.write("404 Данная страница не найдена!");
         res.end();
     }
